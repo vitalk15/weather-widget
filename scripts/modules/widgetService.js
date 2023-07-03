3badf3ffcabf3533b334558906820b8ee2981621
vitalk15 <vital.15@mail.ru>
2023-07-03 23:47:58 +0300

    add citySearch

--- scripts/modules/widgetService.js
+++ scripts/modules/widgetService.js
@@ -1,24 +1,34 @@
-import { fetchForecast, fetchWeather } from './APIservice.js';
-import { renderWidgetForecast, renderWidgetOther, renderWidgetToday } from './render.js';
+import { fetchForecast, fetchWeather, getCity } from './APIservice.js';
+import { renderWidgetForecast, renderWidgetOther, renderWidgetToday, showError } from './render.js';
 
-export const startWidget = async () => {
-	const city = 'Минск';
-	const widget = document.createElement('div');
-	widget.className = 'widget';
+export const startWidget = async (city, widget) => {
+	if (!city) {
+		const dataCity = await getCity();
+		if (dataCity.success) {
+			city = dataCity.city;
+		} else {
+			showError(widget, dataCity.error);
+		}
+	}
+
+	if (!widget) {
+		widget = document.createElement('div');
+		widget.className = 'widget';
+	}
 
 	const dataWeather = await fetchWeather(city);
 	if (dataWeather.success) {
 		renderWidgetToday(widget, dataWeather.data);
 		renderWidgetOther(widget, dataWeather.data);
 	} else {
-		showError(dataWeather.error);
+		showError(widget, dataWeather.error);
 	}
 
 	const dataForecast = await fetchForecast(city);
 	if (dataForecast.success) {
 		renderWidgetForecast(widget, dataForecast.data);
 	} else {
-		showError(dataForecast.error);
+		showError(widget, dataForecast.error);
 	}
 
 	return widget;
